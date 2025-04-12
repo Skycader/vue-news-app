19cee0237cd3e971fe7ec476dd5b5500a493f841
Устименко Даниил <ustimeko_di@crc.ru>
2025-04-12 21:27:36 +0300

    main: ok

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <AppNews v-for="item of news" :key="item" :news="item" />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "./styles/theme.scss";
 </style>
+
+<script lang="ts">
+import AppNews from "./components/AppNews.vue";
+export default {
+  data() {
+    return {
+      news: [
+        "Windows 12 is out",
+        "Linux Kernel 2.1 is out",
+        "Trump has made Elon Musk his son",
+      ],
+    };
+  },
+  components: {
+    AppNews: AppNews,
+  },
+};
+</script>
---
+++ src/components/AppNews.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <h3>{{ news }}</h3>
+    <button class="btn" @click="isOpen = !isOpen">
+      {{ !isOpen ? "Раскрыть" : "Свернуть" }}
+    </button>
+    <p v-if="isOpen">Info...</p>
+  </div>
+  <br />
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  props: {
+    news: {
+      type: String, // Указываем тип свойства
+      required: true, // Указали, что это свойство обязательно
+    },
+  },
+};
+</script>
