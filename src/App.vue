18b1ad363cee9424d25e55e4e801328c9506c45c
Устименко Даниил <ustimeko_di@crc.ru>
2025-04-16 14:52:47 +0300

    main: ok

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,43 @@
 <template>
-  <AppNews v-for="item of news" :key="item" :news="item" />
+  <div>
+    <div class="card">
+      <h3>Мои новости</h3>
+      <hr />
+      <h4>Вы посмотрели {{ views }} статей</h4>
+      <h4>Вы прочитали {{ reads }} статей</h4>
+    </div>
+    <br />
+    <AppNews
+      v-for="item of news"
+      :key="item"
+      :news="item"
+      @item-opened="this.views++"
+      @read="item.read = true"
+    />
+  </div>
 </template>
 
 <style lang="scss">
 @import "./styles/theme.scss";
 </style>
 
-<script lang="ts">
+<script>
 import AppNews from "./components/AppNews.vue";
 export default {
   data() {
     return {
+      views: 0,
       news: [
-        "Windows 12 is out",
-        "Linux Kernel 2.1 is out",
-        "Trump has made Elon Musk his son",
+        { title: "Windows 12 is out", read: false },
+        { title: "Linux Kernel 2.1 is out", read: false },
+        { title: "Trump has made Elon Musk his son", read: false },
       ],
     };
+  },
+  computed: {
+    reads() {
+      return this.news.reduce((total, item) => (total += item.read), 0);
+    },
   },
   components: {
     AppNews: AppNews,
--- src/components/AppNews.vue
+++ src/components/AppNews.vue
@@ -1,20 +1,40 @@
 <template>
   <div class="card">
-    <h3>{{ news }}</h3>
-    <button class="btn" @click="isOpen = !isOpen">
+    <h3>{{ news.title }}</h3>
+    <button class="btn" @click="toggleItem">
       {{ !isOpen ? "Раскрыть" : "Свернуть" }}
     </button>
-    <p v-if="isOpen">Info...</p>
+
+    <div v-if="isOpen">
+      <p>Info...</p>
+      <button
+        class="btn"
+        :class="!news.read ? 'primary' : ''"
+        @click="readTheArticle"
+      >
+        {{ news.read ? "Прочитано" : "Прчитать" }}
+      </button>
+    </div>
   </div>
   <br />
 </template>
 
-<script lang="ts">
+<script>
 export default {
+  emits: ["read", "item-opened"],
   data() {
     return {
       isOpen: false,
     };
+  },
+  methods: {
+    readTheArticle() {
+      this.$emit("read");
+    },
+    toggleItem() {
+      this.isOpen = !this.isOpen;
+      if (this.isOpen) this.$emit("item-opened", true);
+    },
   },
   props: {
     news: {
